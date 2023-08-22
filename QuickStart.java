3cb19ba82c7ad028a5202efc795cccdcfcae846e
mursalin <mursalinlamon@gmail.com>
2023-08-22 23:45:49 +0600

    codes

---
+++ QuickStart.java
@@ -0,0 +1,7 @@
+class QuickStart
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Assalamualaikum,world");
+    }
+}
---
+++ sum_of_two_numbers.java
@@ -0,0 +1,15 @@
+public class sum_of_two_numbers 
+{
+    public static void main(String a[] )
+    {
+        int x,y,sum;
+
+        x=4;
+        y=6;
+        sum=x+y;
+
+        System.out.println(sum);
+        
+    }
+    
+}
