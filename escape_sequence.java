5d1cf51a833c844c8b990a8441615178a1afe32e
mursalin <mursalinlamon@gmail.com>
2023-08-24 21:57:53 +0600

    Create escape_sequence.java

---
+++ escape_sequence.java
@@ -0,0 +1,10 @@
+public class escape_sequence {
+
+    public static void main(String[] args){
+
+        System.out.print("Assalamualaikum, this is mursalin\n");
+        System.out.print(1);
+        System.out.print("\n1");
+    }
+    
+}
