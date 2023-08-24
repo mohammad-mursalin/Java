e6d208d8d0ed852907a53f7f76d7ae2b02aa8a5d
mursalin <mursalinlamon@gmail.com>
2023-08-24 21:49:25 +0600

    codes

---
+++ literals.java
@@ -0,0 +1,20 @@
+public class literals 
+{
+    public static void main( String [] args)
+    {
+        int x=0b1111;
+        int y=0xEF;
+        int a=1_00_00_000;
+        double z=12e5;
+
+        System.out.print("x=");
+        System.out.println(x);
+        System.out.print("y=");
+        System.out.println(y);
+        System.out.print("z=");
+        System.out.println(z);
+        System.out.print("a=");
+        System.out.println(a);
+    }
+    
+}
---
+++ nor_Operator.java
@@ -0,0 +1,13 @@
+public class nor_Operator 
+{
+    public static void main( String a[])
+    {
+        int x=5;
+        int y=6;
+
+        boolean result=x<y;
+
+        System.out.print(!result);
+    }
+    
+}
