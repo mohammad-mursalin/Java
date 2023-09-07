2e626f4da2498ef493c54f589857d03d88d957ca
mursalin <mursalinlamon@gmail.com>
2023-09-07 23:42:12 +0600

    Create DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop 
+{
+    public static void main( String[] args)
+    {
+        int i=6;
+
+        do{
+            System.out.println("Assalamualaikum " +i);
+            i++;
+        }while(i<=5);
+    }
+}
