3101538bbd963a843c6460045d21e4c3fa096e11
mursalin <mursalinlamon@gmail.com>
2023-09-08 22:41:19 +0600

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+public class TernaryOperator 
+{
+    public static void main( String[] args)
+    {
+        int x=4;
+        String result;
+
+        result= x%2==0 ? "even" : "odd";
+
+        System.out.println(result);
+    }
+}
