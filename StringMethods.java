2280c091c1c7f44fdf46a953d8dc8b581570b176
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-17 23:32:34 +0600

    some methods of string

---
+++ StringMethods.java
@@ -0,0 +1,15 @@
+public class StringMethods 
+{
+    public static void main(String[] args) 
+    {
+        String name;
+        name="Mursalin Lamon";
+
+        System.out.println("Welcome " +name);
+        System.out.println("Length of my name is " +name.length());
+        System.out.println("My name in uppercase " +name.toUpperCase());
+        System.out.println("My name in lowercase " +name.toLowerCase());
+        System.out.println("Position of Lamon is " +name.indexOf("Lamon"));
+        
+    }   
+}
