18e5d9ded9cafbe5b7aa57eafd23ae742183ad0c
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-18 23:27:27 +0600

    functions of math

---
+++ Mathfunction.java
@@ -0,0 +1,14 @@
+public class Mathfunction 
+{
+    public static void main(String[] args) 
+    {
+     
+        System.out.println("Maximum number " +Math.max(4,7));
+        System.out.println("Minimum number " +Math.min(4,7));
+        System.out.println("Squre root of number " +Math.sqrt(49));
+        System.out.println("Absulate number " +Math.abs(-5));
+        System.out.println("Random number " +Math.random());
+        System.out.println("Randon number between 0 to 100 " +(int)(Math.random()*101));
+        
+    }   
+}
