4b42463180a57a86e28069596a1797921db14386
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-20 22:53:11 +0600

    use of + operator

---
+++ AddingNumAndString.java
@@ -0,0 +1,10 @@
+public class AddingNumAndString 
+{
+    public static void main(String[] args) 
+    {
+        String num="20";
+        int x=23;
+
+        System.out.println("concatenation of a number and a string is " +x+num);
+    }   
+}
