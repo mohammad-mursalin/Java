cc0a32ef8f46fd8363ba7a255c2303f4a4736321
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-20 22:54:29 +0600

    Array function

---
+++ ForEachLoop.java
@@ -0,0 +1,14 @@
+public class ForEachLoop 
+{
+    public static void main(String[] args) 
+    {
+        String[] name={"Jubayer","Shokti","Arif","Shakkhor"};
+
+        System.out.print("My friends are");
+
+        for(String i:name)
+        {
+            System.out.print(" "+i);
+        }
+    }
+}
