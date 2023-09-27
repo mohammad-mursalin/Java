b593ba5276c1335af05324a8f3aacde08d276960
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-27 23:18:46 +0600

    methods function

---
+++ MethodsBasic.java
@@ -0,0 +1,11 @@
+public class MethodsBasic 
+{
+    static void firstMethod()
+    {
+        System.out.println("Excecuted first method");
+    }
+    public static void main(String[] args) 
+    {
+        firstMethod();
+    }
+}
