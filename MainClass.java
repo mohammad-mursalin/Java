a37016696fd2aa033b609fab9807a528c30677f6
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-28 22:31:50 +0600

    class

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass 
+{
+    public static void main(String[] args) 
+    {
+        SeparateClass ob1 = new SeparateClass();
+
+        System.out.println("My name is " +ob1.name);
+    }
+}
