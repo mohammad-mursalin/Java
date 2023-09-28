634f637a634291cba1ff143487028e54a2c7737a
mohammad-mursalin <mursalinlamon@gmail.com>
2023-09-28 23:06:13 +0600

    java package

---
+++ firstpackage/FirstPackage.java
@@ -0,0 +1,9 @@
+package firstpackage;
+
+public class FirstPackage 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Creating my first package in java");
+    }
+}
