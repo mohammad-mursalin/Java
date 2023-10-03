a560a7e827570f8f8856dbe0c5146e7aba4d51fc
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-03 22:20:29 +0600

    compare two string

---
+++ StringCompare.java
@@ -0,0 +1,10 @@
+public class StringCompare 
+{
+    public static void main(String[] args) 
+    {
+        String name1="mursalin";
+        String name2="shakti";
+
+        System.out.println(name1.equals(name2));
+    }
+}
