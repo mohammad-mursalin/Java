3797fd3a01016fbd7949d2f4fa9a7ebdad96b109
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-10 23:56:06 +0600

    builder class

---
+++ BuilderClass.java
@@ -0,0 +1,26 @@
+public class BuilderClass 
+{
+    public static void main(String[] args) 
+    {
+        StringBuilder name = new StringBuilder("Mursalin lamon");
+        name.append(" 23");
+        System.out.println(name);
+
+        /*StringBuffer name2 = new StringBuffer();
+        name.append("Mursalin");
+        System.out.println(name2);
+
+        String name3 = "Lamon";
+        StringBuffer name4 = new StringBuffer(name3);
+        System.out.println(name4);*/
+
+        name.reverse();
+        System.out.println(name);
+
+        name.delete(0, 5);
+        System.out.println(name);
+
+        name.setLength(4);
+        System.out.println(name);
+    }
+}
