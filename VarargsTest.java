3b961c6a399cb2c1ea245f2bf29e5779f29d1c27
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-14 23:36:22 +0600

    varargs method

---
+++ VarargsTest.java
@@ -0,0 +1,7 @@
+public class VarargsTest {
+    public static void main(String[] args) {
+        VarargsMethod ob = new VarargsMethod();
+        ob.add(3,4);
+        ob.add(43,234,44,12);
+    }
+}
