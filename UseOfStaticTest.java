daa8f076d53bcb1febafafb913ced234875c61af
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-15 15:14:05 +0600

    use of static

---
+++ UseOfStaticTest.java
@@ -0,0 +1,18 @@
+public class UseOfStaticTest 
+{
+    public static void main(String[] args) 
+    {
+        UseOfStatic std1 = new UseOfStatic();
+        std1.displayInfo();
+
+        UseOfStatic std2 = new UseOfStatic();
+        std2.displayInfo();
+
+        UseOfStatic std3 = new UseOfStatic();
+        std3.displayInfo();
+
+        UseOfStatic std4 = new UseOfStatic();
+        std4.displayInfo();
+
+    }
+}
