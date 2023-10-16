c8faefc3d5cc31be95d8bf2be1f5d93f1a9e165d
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-16 23:55:17 +0600

    java debuging

---
+++ Debug.java
@@ -0,0 +1,14 @@
+public class Debug 
+{
+    public static void main(String[] args) 
+    {
+        int i,sum = 0;
+
+        for(i = 1 ; i<=10 ; i++)
+        {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum = " +sum);
+    }
+}
