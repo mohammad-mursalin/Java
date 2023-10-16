9476e7f0ca4a09e40ae9d9c8bfaaa01ab39699d2
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-16 23:56:45 +0600

    java encapsulation

---
+++ EncapTest.java
@@ -0,0 +1,9 @@
+public class EncapTest 
+{
+    public static void main(String[] args) 
+    {
+        EncapClass person = new EncapClass();
+        person.setName("Mursalin");
+        System.out.println("My name is " +person.getName());
+    }
+}
