4fcb9c248b36b3fb9543dd2e90aca84adda75eea
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-17 18:13:43 +0600

    java recursion

---
+++ JavaRecursion1Test.java
@@ -0,0 +1,9 @@
+public class JavaRecursion1Test 
+{
+    public static void main(String[] args) 
+    {
+        JavaRecursion1 ob = new JavaRecursion1();
+        int result = ob.fact(5);
+        System.out.println("Factorial : " +result);
+    }
+}
