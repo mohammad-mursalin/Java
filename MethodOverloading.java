11044bfd9ccbff20cc0967f97f4e7ab83001319f
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-18 23:45:53 +0600

    method overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,26 @@
+public class MethodOverloading {
+    void add()
+    {
+        System.out.println("Without parameter");
+    }
+
+    void add(int a)
+    {
+        System.out.println("With parameter");
+    }
+
+    void add(int a,int b)
+    {
+        System.out.println("With parameter");
+    }
+
+    void add(double a,double b)
+    {
+        System.out.println("With parameter");
+    }
+
+    void add(int a, int b,int c)
+    {
+        System.out.println("With parameter");
+    }
+}
