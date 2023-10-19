c82eeb62d04c3103f5c4c7f9617bfe183dc08e66
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-19 21:34:51 +0600

    static method

---
+++ StaticMethod.java
@@ -0,0 +1,12 @@
+public class StaticMethod
+{
+    void display1()
+    {
+        System.out.println("I'm a non static method");
+    }
+
+    static void display2()
+    {
+        System.out.println("I'm a static method");
+    }
+}
