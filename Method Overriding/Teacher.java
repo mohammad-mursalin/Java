fda910e7c109aa911963125f0bbc682118cb8c94
mohammad-mursalin <mursalinlamon@gmail.com>
2023-10-30 20:17:37 +0600

    method overriding

---
+++ Method Overriding/Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher extends Person 
+{
+    String qualification;
+
+    void display()
+    {
+        System.out.println("Name : " +name);
+        System.out.println("Age : " +age);
+        System.out.println("Qualification : " +qualification);
+    }
+    
+}
---
+++ Method Overriding/Test.java
@@ -0,0 +1,16 @@
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        Teacher t = new Teacher();
+        t.name = "Mursalin";
+        t.age = 23;
+        t.qualification = "BSc in ICE";
+        t.display();
+
+        Person p = new Person();
+        p.name = "Lamon";
+        p.age = 23;
+        p.display();
+    }
+}
