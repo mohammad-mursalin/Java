118d6ff6312f38941f655802a0b651608382dc2f
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-01 23:10:35 +0600

    this keyword

---
+++ ThisKeywordTest.java
@@ -0,0 +1,11 @@
+public class ThisKeywordTest 
+{
+    public static void main(String[] args) 
+    {
+        ThisKeyword p1 = new ThisKeyword("Mursalin", 23);
+        p1.display();
+
+        ThisKeyword p2 = new ThisKeyword("Lamon", 23, "Black");
+        p2.display();
+    }
+}
