acb26cd1000f54b33ee70cc435dbb35d163fe904
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-02 23:58:54 +0600

    Final method

---
+++ Final Keyword/TestFinalMethod.java
@@ -0,0 +1,9 @@
+public class TestFinalMethod {
+    
+    public static void main(String[] args) {
+        
+        FinalMethod2 ob = new FinalMethod2();
+        ob.display();
+        ob.display1();
+    }
+}
