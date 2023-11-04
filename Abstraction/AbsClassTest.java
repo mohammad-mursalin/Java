248c1367d3f3d67889d77019cfa0513f2c667212
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-04 22:03:08 +0600

    abstraction

---
+++ Abstraction/AbsClassTest.java
@@ -0,0 +1,14 @@
+public class AbsClassTest {
+
+    public static void main(String[] args) {
+        
+        AbstractClass ab;
+
+        ab = new Rahim();
+        ab.method();
+
+        ab = new Karim();
+        ab.method();
+    }
+    
+}
