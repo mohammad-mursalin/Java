35b09029a430c46f04df56600bc87f9ded1ddf0a
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-05 21:40:33 +0600

    Interface

---
+++ Interface/AnimalTest.java
@@ -0,0 +1,14 @@
+public class AnimalTest {
+
+    public static void main(String[] args) {
+        
+        Dog d = new Dog();
+        d.eat();
+
+        Cat c = new Cat();
+        c.eat();
+
+        
+    }
+    
+}
