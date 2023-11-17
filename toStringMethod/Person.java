2e91680576f1d4421508e5baef5d8e205c200ee9
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-17 23:30:55 +0600

    toString method

---
+++ toStringMethod/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+
+    String name;
+    int age;
+
+    Person (String name, int age) {
+
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String toString() {
+
+        return "Name : "+name+"\nAge : "+age;
+    }
+}
---
+++ toStringMethod/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        Person p1 = new Person("Mursalin", 23);
+        Person p2 = new Person("mohammad", 23);
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
