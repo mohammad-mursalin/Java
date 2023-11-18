8db2f6d2f9a417cef7461675f31e3e44b7378964
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-18 20:10:51 +0600

    linked list

---
+++ LinkedListDemo.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    
+    public static void main(String[] args) {
+        
+        LinkedList<String> p = new LinkedList<String>();
+
+        p.add("Mursalin");
+        p.add(" Shoktiban");
+
+        System.out.println(p);
+    }
+}
