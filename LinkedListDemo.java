8a0cf967a2de7d77b5bfaf2b748bb6e5b70a2e03
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-18 23:54:20 +0600

    code added

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -5,10 +5,32 @@ public class LinkedListDemo {
     public static void main(String[] args) {
         
         LinkedList<String> p = new LinkedList<String>();
+        LinkedList<String> p1 = new LinkedList<String>();
 
         p.add("Mursalin");
         p.add(" Shoktiban");
 
         System.out.println(p);
+
+        p.add(0, "Toki");
+        p.addFirst("Mohammad");
+        p.addLast("lamon");
+
+        System.out.println(p);
+
+        p1.addAll(p);
+
+        System.out.println(p1);
+
+        p1.remove("lamon");
+        System.out.println(p1);
+
+        p1.remove(3);
+        System.out.println(p1);
+
+        p1.removeAll(p1);
+        System.out.println(p1);
+
+        p.clear();
     }
 }
