1590573f40e19ed04455d5503165c3bf9600d3a1
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-18 23:57:32 +0600

    LinkedList

---
+++ LinkedList/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    String name;
+    int roll;
+
+    Student(String name, int roll) {
+
+        this.name = name;
+        this.roll = roll;
+    }
+}
---
+++ LinkedList/Test.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        LinkedList<Student> list = new LinkedList<Student>();
+
+        Student s1 = new Student("Mursalin",23);
+        Student s2 = new Student("Mohammad",22);
+        Student s3 = new Student("Lamon",24);
+
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+
+        for(Student s : list) {
+
+            System.out.println(s.name+ "  " +s.roll);
+        }
+    }
+}
