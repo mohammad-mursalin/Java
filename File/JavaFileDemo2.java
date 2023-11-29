e8d220d606a5afc72509104c9821d1e67bea7f25
mohammad_mursalin <mursalinlamon@gmail.com>
2023-11-30 00:08:54 +0600

    creating and deleting file

--- File/JavaFileDemo2.java
+++ File/JavaFileDemo2.java
@@ -12,10 +12,28 @@ public class JavaFileDemo2 {
             System.out.println("File is created");
 
         } catch (Exception e) {
-            
+
             System.out.println(e);
         }
 
-        
+        if (file.exists()) {
+            
+            System.out.println("file exists");
+
+        } else {
+            
+            System.out.println("File does not exists");
+        }
+
+        file.delete();
+
+        if (file.exists()) {
+            
+            System.out.println("file exists");
+
+        } else {
+            
+            System.out.println("File does not exists");
+        }
     }
 }
