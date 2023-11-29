df4b36b3e2ae45d5a4782e40bd14e1f4d2db4afe
mohammad_mursalin <mursalinlamon@gmail.com>
2023-11-29 23:39:04 +0600

    method added

--- File/JavaFileDemo1.java
+++ File/JavaFileDemo1.java
@@ -4,11 +4,18 @@ public class JavaFileDemo1 {
 
     public static void main(String[] args) {
         
-        File dir = new File("Test file1");
+        File dir = new File("Test file1");     //it will create this folder in the same package folder
 
         dir.mkdir();
 
         String dirLocation = dir.getAbsolutePath();
-        System.out.println("Location of the file : " +dirLocation);
+        System.out.println("Location of the folder : " +dirLocation);
+
+        System.out.println("Folder name : " +dir.getName());
+
+        if (dir.delete()) {
+            
+            System.out.println(dir.getName()+ " folder is deleted successfully");
+        }
     }
 }
