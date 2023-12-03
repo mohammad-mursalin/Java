b5548d273b670cb98f43c8d25721ad104e5d9c88
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-03 11:41:11 +0600

    Encapsulation concept

---
+++ JavaT.java
@@ -0,0 +1,19 @@
+class Human {
+
+    private String name = "Mursalin";
+    
+    public void show()
+    {
+        System.out.println(name);
+    }
+}
+
+public class JavaT {
+    
+    public static void main(String[] args) {
+        
+        Human ob = new Human();
+        ob.show();
+
+    }
+}
