e4707a34cd15528367528c80b66ea2381e630ef8
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-07 23:40:14 +0600

    for parameter

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -1,13 +1,13 @@
 @FunctionalInterface
 interface Demo {
-    void show();
+    void show();  //void show(int i);
 }
 
 public class LambdaExpression {
     
     public static void main(String[] args) {
 
-        Demo obj = () -> System.out.println("In show");
+        Demo obj = () -> System.out.println("In show");  //(i)
 
         obj.show();
         
