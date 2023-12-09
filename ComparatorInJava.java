5da5339b57bbefe6770baabc204f401ec826d4aa
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-09 11:34:33 +0600

    sorting based on own logic

--- ComparatorInJava.java
+++ ComparatorInJava.java
@@ -2,13 +2,25 @@
 //I'm sortig based on the second number of a number
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ComparatorInJava {
     
     public static void main(String[] args) {
+
+        Comparator<Integer> com = new Comparator<Integer>() {
+            
+            public int compare ( Integer i, Integer j) {
+
+                if(i%10 > j%10)
+                    return 1;
+
+                else
+                    return -1;
+            }
+        };
         
         List<Integer> num = new ArrayList<>();
 
@@ -17,7 +29,9 @@ public class ComparatorInJava {
         num.add(56);
         num.add(73);
 
-        Collections.sort(num);
+        Collections.sort(num,com);
+
+        //Collections.sort(num);
 
         System.out.println(num);
     }
