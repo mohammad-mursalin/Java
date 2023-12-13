bd6c99453b237c353a68f25af9f16f0358822c8c
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-13 22:55:36 +0600

    code added

--- GUIdemo.java
+++ GUIdemo.java
@@ -5,7 +5,8 @@ public class GUIdemo {
     public static void main(String[] args) {
         
         String name = JOptionPane.showInputDialog("Enter your name");
-
         JOptionPane.showMessageDialog(null, "hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
     }
 }
