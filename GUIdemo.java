5121f62d9459e3aa78d1a7708b3b84b27d84f619
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-13 22:45:43 +0600

    GUI demo

---
+++ GUIdemo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class GUIdemo {
+    
+    public static void main(String[] args) {
+        
+        String name = JOptionPane.showInputDialog("Enter your name");
+    }
+}
