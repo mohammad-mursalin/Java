3aa81818fc0ab5b785f17851b200eecbefbe39cd
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-14 22:59:04 +0600

    comment

--- GUIdemo.java
+++ GUIdemo.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class GUIdemo {   //graphical user
+public class GUIdemo {   //graphical user interface
      
     public static void main(String[] args) {
         
