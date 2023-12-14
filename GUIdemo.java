3e1e286c28ff6c0c57d290721a2257c09889c716
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-14 22:57:55 +0600

    comment

--- GUIdemo.java
+++ GUIdemo.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 
-public class GUIdemo {
-    
+public class GUIdemo {   //
+     
     public static void main(String[] args) {
         
         String name = JOptionPane.showInputDialog("Enter your name");
